673bb13d6d6b690ebb169aa14a08fd2e057d2dcf
citiufpe <projetos@citi.org.br>
2018-12-06 20:29:57 -0300

    visual adjustments

--- views/home/home.js
+++ views/home/home.js
@@ -1,6 +1,16 @@
-let navbar = document.getElementById('navbar');
+// NAVBAR
+
+let navbar = document.getElementById('sidebar');
 let hamburguer = document.getElementById('hamburguer');
+let sidebarList = document.getElementById('sidebarList');
 
 hamburguer.addEventListener('click', function () {
     navbar.classList.toggle('active');
+
+    let sidebarListDisplay = window.getComputedStyle(sidebarList).getPropertyValue('display');
+    if (sidebarListDisplay == 'block') {
+        sidebarList.style.display = 'none';
+    } else if (sidebarListDisplay == 'none') {
+        sidebarList.style.display = 'block';
+    }
 });
